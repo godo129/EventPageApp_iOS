27f76c79b433f6978f9a449277224878d58ba8c4
godo <godo129@naver.com>
2021-12-16 01:48:04 +0900

    테스트

--- EventPageApp/Controller/ViewController.swift
+++ EventPageApp/Controller/ViewController.swift
@@ -12,11 +12,18 @@ class ViewController: UIViewController {
     let url = "https://gg6hx0.deta.dev/"
     
     let eventManger = EventManger()
+    
+    let imageView = UIImageView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.addSubview(imageView)
+        imageView.frame = CGRect(x: 50, y: 50, width: 300, height: 300)
+        
+        
         // Do any additional setup after loading the view.
-        eventManger.getEventData(url: url)
+        eventManger.getEventData(shoppingMal: "알라딘")
     }
 
 
