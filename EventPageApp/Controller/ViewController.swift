dd74c1037166f42574755ed42554181cdfa4e811
godo <godo129@naver.com>
2021-12-16 01:32:03 +0900

    테스트

--- EventPageApp/Controller/ViewController.swift
+++ EventPageApp/Controller/ViewController.swift
@@ -8,10 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let url = "https://gg6hx0.deta.dev/"
+    
+    let eventManger = EventManger()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        eventManger.getEventData(url: url)
     }
 
 
